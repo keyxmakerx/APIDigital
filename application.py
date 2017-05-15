e668ef8ffcc74f7b929101ade3065282078531eb
keyxmakerx <linuxkeymaker@gmail.com>
2017-05-15 15:58:59 -0500

    Create application.py

---
+++ application.py
@@ -0,0 +1,17 @@
+inFile = open("data.txt")
+outFile = open("result.txt", "w")
+buffer = []
+keepCurrentSet = True
+for line in inFile:
+    buffer.append(line)
+    if line.startswith("----"):
+        #---- starts a new data set
+        if keepCurrentSet:
+            outFile.write("".join(buffer))
+        #now reset our state
+        keepCurrentSet = False
+        buffer = []
+    elif line.startswith("extractme"):
+        keepCurrentSet = True
+inFile.close()
+outFile.close()
