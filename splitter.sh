5b6f270b4397ed807d7718290e75a1a5d8b54d93
keyxmakerx <linuxkeymaker@gmail.com>
2017-05-15 19:24:37 -0500

    Create splitter.sh
    Help from https://journalxtra.com/linux/huge-text-file-need-to-extract-specific-lines-heres-how/ and http://stackoverflow.com/questions/4651437/how-to-set-a-variable-to-the-output-from-a-command-in-bash

---
+++ splitter.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+File=''
+Split=''
+
+read -p "Please Enter File " File;
+
+WC='wc -l $File'
+
+read -p "There are $WC lines of code"
+
+read -p "Please Enter How many Lines " Split;
+
+split -l $Split $File
